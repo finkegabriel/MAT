7b4d6a90353c836f7c212fe642493c7025c82c1a
finkegabriel <finkegabriel@gmail.com>
2023-11-25 12:30:22 -0700

    init

--- mat.sh
+++ mat.sh
@@ -1,8 +1,8 @@
 
 #Login with token and creds
 # curl --cookie cookie.txt --cookie-jar cookie.txt -H 'Content-Type: application/json' \
-#       -d '{ "_token":"fIpGmcdiexvvXo8QeJxtWk2iHR5krr2vDPMubYG7","user-account":"4257399512"}' \
+#       -d "{ "_token":${TOKEN},"user-account":${USER}}" \
 #       -X POST \
 #       "https://wavemaxlive.com/login"
 
-# curl -b cookie.txt "https://wavemaxlive.com/machines"
+curl -b cookie.txt "https://wavemaxlive.com/machines"
